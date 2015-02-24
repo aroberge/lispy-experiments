06403985207dcc359b0388772c62100a882776f3
André Roberge <andre.roberge@gmail.com>
2015-02-24 09:15:58 -0400

    isolating parser tests into own file

---
+++ test/test_parse.py
@@ -0,0 +1,21 @@
+
+import unittest
+
+from src.parser import Parser
+
+parse = Parser({}, {}).parse
+
+
+class TestParse(unittest.TestCase):
+    '''Ensures that we parse expressions correctly, transforming them into
+       the appropriate "list of lists" representation'''
+
+    def test_parse_add(self):
+        self.assertEqual(['+', 3, 4], parse("(+ 3 4)"), msg="basic")
+        self.assertEqual(['+', 3, 4], parse(" ( + 3  4  ) "), msg="extra spaces")
+
+    def test_parse_add_more(self):
+        self.assertEqual(['+', 3, 4, 5], parse(" ( + 3 4 5)"), msg="more args")
+
+    def test_parse_two_levels(self):
+        self.assertEqual(['*', ['+', 3, 4], ['-', 2, 1]], parse(" (* ( + 3 4) (- 2 1))"))
--- test/test_petit.py
+++ test/test_petit.py
@@ -12,21 +12,6 @@ def evaluate(s):
     return pl.evaluate(parse(s))
 
 
-class TestParse(unittest.TestCase):
-    '''Ensures that we parse expressions correctly, transforming them into
-       the appropriate "list of lists" representation'''
-
-    def test_parse_add(self):
-        self.assertEqual(['+', 3, 4], parse("(+ 3 4)"), msg="basic")
-        self.assertEqual(['+', 3, 4], parse(" ( + 3  4  ) "), msg="extra spaces")
-
-    def test_parse_add_more(self):
-        self.assertEqual(['+', 3, 4, 5], parse(" ( + 3 4 5)"), msg="more args")
-
-    def test_parse_two_levels(self):
-        self.assertEqual(['*', ['+', 3, 4], ['-', 2, 1]], parse(" (* ( + 3 4) (- 2 1))"))
-
-
 class TestLoadFile(unittest.TestCase):
     '''Simple test to see if we load files correctly'''
 
