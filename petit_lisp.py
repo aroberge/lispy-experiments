440feccab9c5b91778a0f6260d5d7da99efdf633
André Roberge <andre.roberge@gmail.com>
2015-02-24 07:55:19 -0400

    putting Env back into main script

--- petit_lisp.py
+++ petit_lisp.py
@@ -8,11 +8,26 @@ from src.parser import Parser
 from src.repl import InteractiveInterpreter
 from src.lisps import lisp_procs
 from src.procedures import Procedure
-from src.environment import Env
-
 
 loader = FileLoader()
 STRINGS = {}
+
+
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs, with an outer Env."
+
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params, args))
+        self.outer = outer
+
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        if var in self:
+            return self
+        elif self.outer is not None:
+            return self.outer.find(var)
+        else:
+            raise ValueError("{} is not defined".format(var))
 
 
 def display(s):
