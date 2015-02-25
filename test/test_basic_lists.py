c54da34500e95150c2ef9c6057339525edf1e03f
André Roberge <andre.roberge@gmail.com>
2015-02-25 17:41:55 -0400

    adding test for basic lisp construct

--- test/test_basic_lists.py
+++ test/test_basic_lists.py
@@ -10,16 +10,18 @@ def evaluate(s):
     return pl.evaluate(parse(s))
 
 
-class TestLists(unittest.TestCase):
+class TestBasicLisp(unittest.TestCase):
 
     def setUp(self):  # noqa
         pl.global_env = pl.common_env(pl.Env())
+        self.assertEqual(True, evaluate("(undefined? a)"))
 
     def test_cons(self):
         expr = "(define a (cons 1 (cons 2 (cons 3 (cons 4 '())))))"
         expr2 = "'(1 2 3 4)"
         evaluate(expr)
         self.assertEqual(evaluate(expr2), evaluate("a"))
+        self.assertEqual(False, evaluate("(undefined? a)"))
 
     def test_cons_error(self):
         expr = "(define a (cons 1 '2))"
@@ -36,3 +38,7 @@ class TestLists(unittest.TestCase):
         expr2 = "(cdr a)"
         evaluate(expr)
         self.assertEqual(evaluate("'(2 3 4)"), evaluate(expr2))
+
+    def test_is_atom(self):
+        self.assertEqual(True, evaluate("(atom? 3)"))
+        self.assertEqual(False, evaluate("(atom? '(1 2 3))"))
